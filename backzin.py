56f235aae9186248bc20435a6073ead6c7fed0a9
Kamily Matias <kleberisaac900@gmail.com>
2026-02-19 08:17:11 -0300

    cxzc

---
+++ backzin.py
@@ -0,0 +1,9 @@
+nome = 'Kleber'
+idade = 17
+kg = 83
+print(nome, idade, kg)
+
+nome = input('Qual seu nome?: ')
+Idade = input('Qual sua idade?: ')
+peso = input('Qual seu peso?: ')
+print('O seu nome é {}, sua idade é {} e seu peso é'.format(nome, idade, peso))
